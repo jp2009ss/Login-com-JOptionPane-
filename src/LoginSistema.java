965290a4d10f1bf1fbc65f43f6025301f95885f5
Joao Pedro de Souza Santos <1094296@senacminas.edu.br>
2026-04-17 21:38:06 -0300

    Login

--- src/LoginSistema.java
+++ src/LoginSistema.java
@@ -1,5 +1,28 @@
-
+import javax.swing.JOptionPane;
 
 public class LoginSistema {
-
+    public static void main(String[] args) {
+        
+        int opcao;
+        
+        do{
+            String[] opcoes = {"Entrar", "Sair"};
+            opcao = JOptionPane.showOptionDialog(null, "Escolha um opção", "Sistem de Login", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opcoes, opcoes[0]);
+            
+            if(opcao == 0){
+                realizarLogin();
+        }
+    }while(opcao == 0);
+}
+    
+public static void realizarLogin(){
+    int tentativas = 0;
+    final int MAX_TENTATIVAS = 3
+            
+            while(tentativas < MAX_TENTATIVAS){
+                String login = JOptionPane.showInputDialog("Digite o login");
+                if (){
+                    s
+                }
+            }
 }
